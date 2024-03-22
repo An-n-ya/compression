493a3dda3f65d48bc66339a69a6cbeff965127f6
Annya <annya.host@gmail.com>
2024-03-22 21:25:31 +0800

    handle c3 situation, but we still don't know how to end encoding

--- src/arithmetic_codec.rs
+++ src/arithmetic_codec.rs
@@ -38,19 +38,7 @@ impl FloatCodec {
             let fx_r = fx.get_val(c);
             (l, u) = (l + (u - l) * fx_l, l + (u - l) * fx_r);
             assert!(l < u);
-            if l < 0.5 && u > 0.5 {
-                while l > 0.25 && u < 0.75 {
-                    (l, u) = (2.0 * (l - 0.25), 2.0 * (u - 0.25));
-                    e3_cnt += 1;
-                }
-                println!(
-                    "c: {c} fx_l:{fx_l:.4} fx_r:{fx_r:.4}, l:{l:.4}, u:{u:.4}, e3_cnt:{e3_cnt}"
-                );
-                cur_index += 1;
-                continue;
-            }
-            println!("c: {c} fx_l:{fx_l:.4} fx_r:{fx_r:.4}, l:{l:.4}, u:{u:.4}");
-            while l > 0.5 || u < 0.5 {
+            while l > 0.5 || u < 0.5 || l < 0.5 && l > 0.25 && u > 0.5 && u < 0.75 {
                 if l > 0.5 {
                     handler.write_bit_back(true);
                     (l, u) = (2f64 * (l - 0.5), 2f64 * (u - 0.5));
@@ -65,8 +53,13 @@ impl FloatCodec {
                         handler.write_bit_back(true);
                         e3_cnt -= 1;
                     }
+                } else if l > 0.25 && l < 0.5 && u > 0.5 && u < 0.75 {
+                    (l, u) = (2.0 * (l - 0.25), 2.0 * (u - 0.25));
+                    e3_cnt += 1;
                 }
             }
+            println!("c: {c} fx_l:{fx_l:.4} fx_r:{fx_r:.4}, l:{l:.4}, u:{u:.4} e3_cnt:{e3_cnt}");
+            println!("{handler:?}");
             cur_index += 1;
         }
         handler.write_bit_back(true);
@@ -86,7 +79,10 @@ impl FloatCodec {
 
         while !reader.is_empty() {
             let proportion = (cur_label - l) / (u - l);
-            println!("cur_label:{cur_label}, l:{l:.4}, u:{u:.4} proportion: {proportion:.4}");
+            println!(
+                "cur_label:{cur_label}({}), l:{l:.4}, u:{u:.4} proportion: {proportion:.4}",
+                reader.cur_bit()
+            );
             let c = fx.find_char(proportion);
             println!("find char {c}");
             res.push(c);
@@ -95,19 +91,23 @@ impl FloatCodec {
             }
             let (fx_l, fx_r) = (fx.get_prev_val(c), fx.get_val(c));
             (l, u) = (l + (u - l) * fx_l, l + (u - l) * fx_r);
-            println!("l:{l:.4}, u:{u:.4}, fx_l:{fx_l:.4}, fx_r:{fx_r:.4}");
             assert!(l < u);
-            if l < 0.5 && u > 0.5 {
-                continue;
-            }
-            while l > 0.5 || u < 0.5 {
+            while l > 0.5 || u < 0.5 || l < 0.5 && l > 0.25 && u > 0.5 && u < 0.75 {
                 cur_label = reader.next();
                 if l > 0.5 {
-                    (l, u) = (2f64 * (l - 0.5), 2f64 * (u - 0.5));
+                    (l, u) = (2.0 * (l - 0.5), 2.0 * (u - 0.5));
                 } else if u < 0.5 {
-                    (l, u) = (2f64 * l, 2f64 * u);
+                    (l, u) = (2.0 * l, 2.0 * u);
+                } else if l < 0.5 && l > 0.25 && u > 0.5 && u < 0.75 {
+                    (l, u) = (2.0 * (l - 0.25), 2.0 * (u - 0.25));
+                    if cur_label > 0.5 {
+                        cur_label -= 0.5;
+                    } else {
+                        cur_label += 0.5;
+                    }
                 }
             }
+            println!("l:{l:.4}, u:{u:.4}, fx_l:{fx_l:.4}, fx_r:{fx_r:.4}");
         }
 
         res
@@ -140,6 +140,21 @@ impl FloatReader {
     }
     pub fn cur_val(&self) -> f64 {
         self.cur_val
+    }
+    pub fn cur_bit(&self) -> String {
+        let mut val = self.cur_val;
+        let mut base = 0.5;
+        let mut res = "".to_string();
+        for _ in 0..self.symbol_size {
+            if val >= base {
+                res.push('1');
+                val -= base;
+            } else {
+                res.push('0');
+            }
+            base /= 2.0;
+        }
+        res
     }
     pub fn next(&mut self) -> f64 {
         self.cur_val *= 2.0;
@@ -197,7 +212,7 @@ impl Fx {
             .unwrap()
     }
     pub fn find_char(&self, f: f64) -> char {
-        assert!(f <= 1.0);
+        assert!(f <= 1.0 && f >= 0.0);
         for i in 1..self.freq.len() {
             let cur_f = *self.freq.get(&i).unwrap();
             // FIXME: `>=` or `>`?
@@ -216,8 +231,8 @@ mod tests {
     #[test]
     fn test_encode() {
         let freq = vec![('a', 0.8f64), ('b', 0.02f64), ('c', 0.18f64)];
-        let mut codec = FloatCodec::new_with_freq(6, freq);
-        let input = "acba";
+        let mut codec = FloatCodec::new_with_freq(8, freq);
+        let input = "acbaa";
         let handler = codec.encode(input);
         println!("{handler:?}");
         let res = codec.decode(handler);
